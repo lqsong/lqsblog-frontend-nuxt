b895c8de38e28cef5a59186acc662d35a4d998af
lqs <957698457@qq.com>
2022-05-09 19:49:48 +0800

    Modify statistical code

--- plugins/cnzz.js
+++ plugins/cnzz.js
@@ -10,7 +10,8 @@ if (process.client && process.env.NODE_ENV === 'production') {
       document.getElementById('cnzz_stat') && document.getElementById('cnzz_stat').remove()
 
       const cnzzScript = document.createElement('script')
-      cnzzScript.src = 'https://s5.cnzz.com/z_stat.php?id=1275873331&web_id=1275873331&t=' + new Date().getTime()
+      // cnzzScript.src = 'https://s5.cnzz.com/z_stat.php?id=1275873331&web_id=1275873331&t=' + new Date().getTime()
+      cnzzScript.src = 'https://hm.baidu.com/hm.js?fbad933e32f60c6e1522c7313e0808f8&t=' + new Date().getTime()
       cnzzScript.id = 'cnzz_stat'
       const s = document.getElementsByTagName('script')[0]
       s.parentNode.insertBefore(cnzzScript, s)
